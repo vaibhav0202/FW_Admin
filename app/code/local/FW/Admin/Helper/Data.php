2b26669a805a007d9e9dcac5bf759d3defa8c419
vaibhav0202 <vaibhav.s@cisinlabs.com>
2017-08-23 12:30:52 +0530

    fw admin added

---
+++ app/code/local/FW/Admin/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category    FW
+ * @package     FW_Admin
+ * @copyright   Copyright (c) 2013 F+W Media, Inc. (http://www.fwmedia.com)
+ * @author      J.P. Daniel <jp.daniel@fwmedia.com>
+ */
+class FW_Admin_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
